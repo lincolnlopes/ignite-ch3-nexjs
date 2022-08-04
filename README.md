df8a4dde06ba2d459ab7473ff346a76e46aa25ab
Lincoln Lopes <lincoln.lopes@sicoobengecred.com.br>
2022-08-03 23:15:38 -0300

    CH3.01.01 - Estrutura da aplicação

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 npx create-next-app ignite-ch3-nexjs --template typescript
 
-npm add -D @types/react @types/node 
+npm add -D @types/react @types/node
 npm add sass
 
+https://paulintrognon.fr/blog/typescript-prettier-eslint-next-js
 npm i -D @typescript-eslint/eslint-plugin
-
 npm i -D prettier eslint-config-prettier
+
+https://icons8.com/icons/set/news-favicon
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,12 @@ import styles from '../styles/Home.module.scss';
 export default function Home() {
   return (
     <>
-    <Head><title>IG News | ig.news</title></Head>
-       <h1 className={styles.title}>Hello World</h1>
+      <Head>
+        <title>IG News | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello <span>World</span>
+      </h1>
     </>
-  )
+  );
 }
